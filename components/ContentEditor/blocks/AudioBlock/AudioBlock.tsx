03a75242d9a52ebe2c4af3502c22f7cfa272e579
moxxuk <mark@idigitalmarketing.co.uk>
2022-07-05 23:45:49 +0100

    Add new Audio block components;

---
+++ components/ContentEditor/blocks/AudioBlock/AudioBlock.tsx
@@ -0,0 +1,23 @@
+import {
+  FunctionComponent, useEffect, useState
+} from 'react';
+import ResizeableElement from '../common/ResizeableElement';
+// import urlParser from "js-audio-url-parser";
+
+export const AudioBlock: FunctionComponent = ({block}) => {
+
+  return (
+    <div className="aspect-w-16 aspect-h-9 px-1">
+      <iframe 
+        src={block?.properties?.url} 
+        width="640" 
+        height="360" 
+        frameBorder="0" 
+        allow="autoplay; fullscreen; picture-in-picture" 
+        allowFullScreen
+      />
+    </div>
+  );
+}
+
+export default AudioBlock
