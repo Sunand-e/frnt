c49c805e5854a0992d67c98db2573ce6b5c7ac95
moxxuk <mark@idigitalmarketing.co.uk>
2022-07-22 22:11:54 +0100

    Refactor admin dashboard user panel into `WelcomeUserPanel` for use on user dashboard; refactor 'Pick up where you left off...' message box into `ResumeRecentActivity` component; refactor and tidy admin dashboard;

---
+++ components/dashboard/ResumeRecentActivity.tsx
@@ -0,0 +1,16 @@
+import Button from "../Button"
+import NoticeBox from "../NoticeBox"
+
+const ResumeRecentActivity = () => {
+  
+  return (
+    <NoticeBox>
+    <div className="flex justify-between items-center">
+      <h1 className="font-bold text-lg"><span>Pick up where you left off:</span> <em className="text-main">Know your why</em></h1>
+      <Button>Start now</Button>
+    </div>
+    </NoticeBox>
+  )
+}
+
+export default ResumeRecentActivity
