0185fe019ec8753b906237310d4717e170792a1c
moxxuk <mark@idigitalmarketing.co.uk>
2022-07-28 14:18:13 +0100

    Add 401 response feedback error to `LoginForm`

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,7 +1,7 @@
 import Router from 'next/router';
 import { Form, Formik } from 'formik';
 import { isLoggedInVar } from '../graphql/cache';
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from 'react-hook-form';
 import TextInput from "./common/inputs/TextInput";
 
@@ -15,8 +15,10 @@ interface UserLoginFormValues{
 
 const LoginForm = () => {
 
+  // const [error, setError] = useState(null)
+
   // const isLoggedIn = useReactiveVar(isLoggedInVar);
-    const { register, handleSubmit, control, formState: { errors } } = useForm<UserLoginFormValues>({      });
+    const { setError, register, handleSubmit, control, formState: { errors } } = useForm<UserLoginFormValues>({      });
    // const onSubmit=(data) => console.log(data);
   const onSubmit = values => {
     const data = {
@@ -41,6 +43,14 @@ const LoginForm = () => {
           localStorage.setItem('token', result.token as string);
           isLoggedInVar(true);
           Router.push('/');
+        } else if(result.error) {
+          console.log('result')
+          console.log(result)
+          setError('password', {
+            type: "server",
+            message: 'Username or password is incorrect',
+          });
+          // setError(result.error)
         }
       },
       // Note: it's important to handle errors here
@@ -61,55 +71,56 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <TextInput
-            label="Email address"
-            placeholder="Email address"
-            inputAttrs={register("email", {
-                required:"Email is required",
-                maxLength: {
-                    value: 40,
-                    message:"Max length of the name is 40"
-                },
-                pattern:{
-                    value:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
-                    message: "Please give valid email"
-                }
-            })}
-        />
-        {errors.email && (<small className="text-danger text-red-500">{errors.email.message}</small>)}
-        <TextInput
-            label="Password"
-            placeholder="Password"
-            type="password"
-            className="mt-6"
-            inputAttrs={register("password", {
-                required:"Password is required"
-            })}
-        />
-        <div className="flex items-center justify-between my-6">
-                   <div className="flex items-center">
-                       <input
-                         id="remember-me"
-                         type="checkbox"
-                         className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                         {...register("remember-me")}
-                       />
-                       <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                         Remember me
-                       </label>
-                     </div>
+      <TextInput
+        label="Email address"
+        placeholder="Email address"
+        inputAttrs={register("email", {
+          required:"Email is required",
+          maxLength: {
+            value: 40,
+            message:"Max length of the name is 40"
+          },
+          pattern:{
+            value:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+            message: "Please give valid email"
+          }
+        })}
+      />
+      {errors.email && (<small className="text-danger text-red-500">{errors.email.message}</small>)}
+      <TextInput
+        label="Password"
+        placeholder="Password"
+        type="password"
+        className="mt-6"
+        inputAttrs={register("password", {
+            required:"Password is required"
+        })}
+      />
+      {errors.password && (<small className="text-danger text-red-500">{errors.password.message}</small>)}
+      <div className="flex items-center justify-between my-6">
+        <div className="flex items-center">
+          <input
+            id="remember-me"
+            type="checkbox"
+            className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+            {...register("remember-me")}
+          />
+          <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+            Remember me
+          </label>
+        </div>
 
-                     <div className="text-sm">
-                       <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                         Forgot your password?
-                       </a>
-                     </div>
-                   </div>
-        <button
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-            Sign In
-        </button>
+        <div className="text-sm">
+          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+            Forgot your password?
+          </a>
+        </div>
+      </div>
+      <button
+        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Sign In
+      </button>
     </form>
   )
 }
