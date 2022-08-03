ecd0e5a98fb965f1e2256def63ce322facf40c7f
vijay <vijay.hr@kreatio.com>
2022-08-03 13:18:38 +0530

    Vijay | Upgraded tailwind v2 to v3 and varient is removed from tailwind.config.js fixed #54

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,14 +61,6 @@ module.exports = {
       backgroundOpacity: ['hover', 'active', 'focus'],
     },
   },
-  variants: {
-    extend: {
-      fontWeight: ['hover', 'focus'],
-      zIndex: ['hover', 'active'],
-      flexGrow: ['before', 'after'],
-    },
-    scrollbar: ['rounded']
-  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
