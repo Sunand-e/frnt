d0fa5e68cb0653dab62ea1bef679ab01c8f6945c
moxxuk <mark@idigitalmarketing.co.uk>
2022-08-11 11:39:05 +0100

    Fix widths and padding of `Header` component;

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -10,17 +10,19 @@ export default function Header() {
 
   return (
     <>
-      <div className="sticky top-0 z-20 w-full h-18 mx-auto bg-white flex bg-white shadow-sm justify-between">
-        <div className="flex-none pl-4 lg:pl-6 pl-20 xl:pl-8 flex items-center border-b border-gray-200 lg:border-b-0">
-          <PageTitle />
-            {/* <a className="overflow-hidden w-auto text-lg text-main-secondary font-bold" href="/"> */}
-              {/* {` isAdmin: ${view.isAdmin ? 'on' : 'off'}.`} */}
-            {/* </a> */}
-        </div>
-        <div className="flex items-center justify-between px-4 sm:px-6 lg:mx-6 lg:px-0 xl:mx-8">
-            <HeaderButtons />
-            <Profile />
+      <div className="sticky top-0 z-20 w-full mx-auto bg-white flex shadow-sm">
+        <div className="h-18 mx-auto flex justify-between max-w-screen-2xl w-full px-4 lg:px-6 xl:px-8 ">
+          <div className="flex-none flex items-center border-b border-gray-200 lg:border-b-0">
+            <PageTitle />
+              {/* <a className="overflow-hidden w-auto text-lg text-main-secondary font-bold" href="/"> */}
+                {/* {` isAdmin: ${view.isAdmin ? 'on' : 'off'}.`} */}
+              {/* </a> */}
+          </div>
+          <div className="flex items-center justify-between px-4 sm:px-6 lg:mx-6 lg:px-0 xl:mx-8">
+              <HeaderButtons />
+              <Profile />
 
+          </div>
         </div>
       </div>
 
