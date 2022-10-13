058543fa2e13cbe083464aa6fd1167685483ea8d
moxxuk <mark@idigitalmarketing.co.uk>
2022-10-13 09:43:23 +0100

    Update cache correctly after adding new Pathway:

--- hooks/pathways/useCreatePathway.ts
+++ hooks/pathways/useCreatePathway.ts
@@ -1,24 +1,29 @@
 import { useMutation } from "@apollo/client"
 import { CREATE_PATHWAY } from "../../graphql/mutations/pathway/CREATE_PATHWAY";
 import { CreatePathway, CreatePathwayVariables } from "../../graphql/mutations/pathway/__generated__/CreatePathway";
+import { GET_PATHWAYS } from "../../graphql/queries/allQueries";
+import { GetPathways } from "../../graphql/queries/__generated__/GetPathways";
 
 function useCreatePathway() {
   
   const [createPathwayMutation, createPathwayResponse] = useMutation<CreatePathway, CreatePathwayVariables>(
     CREATE_PATHWAY,
     {
-      // update(cache, { data: { createPathway } } ) {
-        
-      //   const data = cache.readQuery<GetPathways>({
-      //     query: GET_PATHWAYS
-      //   })
-      //   cache.writeQuery({
-      //     query: GET_PATHWAYS,
-      //     data: { 
-      //       pathways: [createPathway.pathway, ...data.pathways]
-      //     }
-      //   })
-      // }
+      update(cache, { data: { createPathway } } ) {
+        const cachedData = cache.readQuery<GetPathways>({
+          query: GET_PATHWAYS
+        })
+        cache.writeQuery({
+          query: GET_PATHWAYS,
+          data: {
+            ...cachedData,
+            pathways: {
+              ...cachedData.pathways,
+              edges: [{node: createPathway.pathway}, ...cachedData.pathways.edges]
+            }            
+          }
+        })
+      },
     }
   );
 
