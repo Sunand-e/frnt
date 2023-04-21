fc4bce8586d6673b56b73e1d9ea6a980d474aac6
moxxuk <mark@idigitalmarketing.co.uk>
2023-04-21 02:47:39 +0100

    Fix SCORM view

---
+++ components/courses/scorm/ScormModuleEdit.tsx
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import useUpdateLesson from '../../../hooks/lessons/useUpdateLesson';
+import { useRouter } from '../../../utils/router';
+import { useBlockStore } from '../../common/ContentEditor/useBlockStore';
+import { useSaveContentButton } from '../../common/ContentEditor/useSaveContentButton';
+import ScormView from './ScormView';
+
+const ScormModuleEdit = () => {
+
+  const router = useRouter()
+  const { cid: id } = router.query
+  const blocks = useBlockStore(state => state.blocks)
+  const isDirty = useBlockStore(state => state.isDirty)
+  const setIsDirty = useBlockStore(state => state.setIsDirty)
+  const content = { blocks }
+
+  const { updateLesson } = useUpdateLesson()
+
+  const handleSave = useCallback(async () => {
+    await updateLesson(id)({content}).then(res => {
+      setIsDirty(false)
+    })
+  },[content, isDirty, updateLesson])
+
+  useSaveContentButton({buttonText: 'Save SCORM Module', isDirty, onSave: handleSave})
+
+  return (
+    <ScormView isEditing={true} />
+  );
+};
+
+export default ScormModuleEdit
