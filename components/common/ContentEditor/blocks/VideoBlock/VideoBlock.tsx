477527e11e1ef0ca7db1186209f78c377a15f94c
moxxuk <mark@idigitalmarketing.co.uk>
2023-07-13 22:16:16 +0100

    Apply width settings in `VideoBlock`;

--- components/common/ContentEditor/blocks/VideoBlock/VideoBlock.tsx
+++ components/common/ContentEditor/blocks/VideoBlock/VideoBlock.tsx
@@ -1,12 +1,18 @@
 export const VideoBlock = ({block}) => {
 
+  const width = block?.properties?.width || '50%';
+
   return (
-    <div className="aspect-video px-1">
+    <div 
+      className="aspect-video px-1 mx-auto"
+      style={{width}}
+    >
+
       <iframe 
         src={block?.properties?.url} 
         className="w-full h-full"
-        width="640" 
-        height="360" 
+        // width="640"
+        // height="360" 
         frameBorder="0" 
         allow="autoplay; fullscreen; picture-in-picture" 
         allowFullScreen
