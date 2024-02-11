ab2dc555b43d831cda9276dd509ce5c5005218a0
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-02-11 22:12:22 +0000

    Update role checks for admin course page;

--- pages/admin/courses/index.tsx
+++ pages/admin/courses/index.tsx
@@ -30,7 +30,7 @@ const AdminCourses = () => {
   
   useEffect(() => {
     if(ready) {
-      if(!userHasCapability('UpdateCourse')) {
+      if(!(userHasCapability('UpdateCourse') || userHasCapability('ReorderCourses'))) {
         router.push('/')
       }
     }
