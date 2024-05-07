f19859591cd421484cd243cba2509136c68b3bbb
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-05-07 16:07:53 +0100

    refactor: Update EnrolUserInContent component to include course and group information

--- components/users/content/EnrolUserInContent.tsx
+++ components/users/content/EnrolUserInContent.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react"
 import useEnrolUsersInContent from "../../../hooks/contentItems/useEnrolUsersInContent"
+import useGetCourses from "../../../hooks/courses/useGetCourses"
+import useGetGroups from "../../../hooks/groups/useGetGroups"
 import useGetRoles from "../../../hooks/roles/useGetRoles"
+import useGetCurrentUser from "../../../hooks/users/useGetCurrentUser"
+import useGetUserCourses from "../../../hooks/users/useGetUserCourses"
 import { closeModal } from "../../../stores/modalStore"
 import Button from "../../common/Button"
 import ContentSelectCategorised from "../../common/inputs/ContentSelectCategorised"
 
-const EnrolUserInContent = ({user, content, assignedContent, typeName='item'}) => {
-  
+const EnrolUserInContent = ({user, typeName='item'}) => {
+
+  const { courses } = useGetCourses()
+  const { groups } = useGetGroups()
+  const { user: currentUser } = useGetCurrentUser()
   const {enrolUsersInContent} = useEnrolUsersInContent()
-
-  const courseNodes = content?.edges.map(edge => edge.node)
-
+  const { courses: assignedContent } = useGetUserCourses(user.id)
+  
   const userCourseNodes = assignedContent?.edges.filter(edge => (
     !edge.node._deleted
      && (
@@ -19,9 +25,33 @@ const EnrolUserInContent = ({user, content, assignedContent, typeName='item'}) =
     )
   )).map(edge => edge.node)
 
-  const availableContent = courseNodes?.filter(course => 
-    !userCourseNodes?.some(userCourse=>userCourse.id === course.id)
+  const userGroupIds = user.groups.edges.map(edge => edge.groupId)
+  const currentUserGroupIds = currentUser.groups.edges.map(edge => edge.groupId)
+  const commonGroupIds = userGroupIds.filter(groupId => currentUserGroupIds.includes(groupId))
+
+  const commonGroupProvisionedCourses = groups?.edges.flatMap(
+    edge => commonGroupIds.includes(edge.node.id) ? edge.node.provisionedCourses.edges : []
   ) || []
+
+  const commonGroupProvisionedCourseNodes = commonGroupProvisionedCourses.map(edge => edge.node)
+  const currentUserEnrolledCourseNodes = courses?.edges.map(edge => edge.node) || []
+
+  console.log('commonGroupProvisionedCourseNodes')
+  console.log(commonGroupProvisionedCourseNodes)
+  
+  const availableCourseNodes = [
+    ...commonGroupProvisionedCourseNodes,
+    ...currentUserEnrolledCourseNodes
+  ]
+
+  console.log('availableCourseNodes')
+  console.log(availableCourseNodes)
+
+  const availableContent = [
+    ...availableCourseNodes?.filter(course => 
+      !userCourseNodes?.some(userCourse=>userCourse.id === course.id)
+    ) || [],
+  ]
   
   const {roles} = useGetRoles()
 
@@ -29,22 +59,25 @@ const EnrolUserInContent = ({user, content, assignedContent, typeName='item'}) =
 
   const [selectedContentIds, setSelectedContentIds] = useState([])
 
-  const handleChange = (items, actionMeta) => {
+  const handleChange = (items) => {
     setSelectedContentIds(items.map(item => item.value))
   }
 
   const handleEnrol = (e) => {
     enrolUsersInContent({
-      userIds: [user.id],
+      userIds: user.id,
       contentItemIds: selectedContentIds,
       roleId: defaultRole?.id
     }, () => {
       closeModal()
     })
   }
-  
+  console.log('availableContent')
+  console.log(availableContent)
   return (
     <>
+
+    
       {
         availableContent?.length ? (
           <div>
@@ -61,7 +94,7 @@ const EnrolUserInContent = ({user, content, assignedContent, typeName='item'}) =
           </div>
         ) : (
           <div className="flex flex-col items-center">
-            No {typeName}s available for enrolment
+            No {typeName}s available to assign
             <Button onClick={closeModal}>OK</Button>
           </div>
         )
