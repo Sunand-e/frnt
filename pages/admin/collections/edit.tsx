e952bdeb4b931bdc14632c91e9cd5c89b82c9999
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-07-24 15:28:02 +0100

    Improve page titles for tag edit pages

--- pages/admin/collections/edit.tsx
+++ pages/admin/collections/edit.tsx
@@ -1,10 +1,35 @@
 import useHeaderButtons from '../../../hooks/useHeaderButtons';
 import EditTagForm from '../../../components/tags/EditTagForm';
 import ButtonBack from '../../../components/common/ButtonBack';
+import {ChevronRight} from "@styled-icons/boxicons-regular/ChevronRight";
+import { useRouter } from 'next/router';
+import useGetTag from '../../../hooks/tags/useGetTag';
+import { ReactNode } from 'react';
+import Link from 'next/link';
+import usePageTitle from '../../../hooks/usePageTitle';
 
 const AdminUsersTagsEdit = () => {
   
-  useHeaderButtons({
+  const router = useRouter()
+  const { id } = router.query
+  const { tag } = useGetTag(id)
+  
+  let pageTitle: ReactNode = 'Collections'
+
+  if(tag) {
+    pageTitle = (
+      <span className='flex items-center space-x-2'>
+        <Link href="/admin/collections">
+          <span>Collections</span>
+        </Link>
+        <ChevronRight size={20} />
+        <span>{tag.label}</span>
+      </span>
+    )
+  }
+
+  usePageTitle({ title: pageTitle })
+    useHeaderButtons({
     id: 'backToCollections',
     component: <ButtonBack text='Back to collections list' action='/admin/collections' />,
   })
