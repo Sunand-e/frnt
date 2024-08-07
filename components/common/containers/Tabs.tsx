8099e704bb3e09efebfc5ed2c370a570cb6c7ffd
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-08-07 16:58:14 +0100

    Update Tabs component to improve tab styling

--- components/common/containers/Tabs.tsx
+++ components/common/containers/Tabs.tsx
@@ -1,7 +1,6 @@
 import classNames from "../../../utils/classNames"
 
 const Tabs = ({tabs, className, activeTab, setActiveTab}) => {
-
   
   return (
     <div className={className}>
@@ -24,7 +23,7 @@ const Tabs = ({tabs, className, activeTab, setActiveTab}) => {
       </div>
       <div className="hidden sm:block">
         <div className="border-b border-gray-200">
-          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+          <nav className="-mb-px flex space-x-4" aria-label="Tabs">
             {tabs.map((tab, index) => (
               <a
                 key={tab.name}
@@ -34,21 +33,26 @@ const Tabs = ({tabs, className, activeTab, setActiveTab}) => {
                   tab.name === activeTab
                     ? 'border-main text-main-secondary'
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200',
-                  'whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer'
+                  'whitespace-nowrap flex py-2 font-medium text-sm cursor-pointer items-center'
                 )}
                 aria-current={tab.name === activeTab ? 'page' : undefined}
               >
-                {tab.title}
-                {tab.count ? (
+                <span className={classNames(
+                  tab.name === activeTab ? 'bg-main-lightness-65 text-white' : 'bg-main-lightness-95 border-transparent text-gray-500',
+                  'px-3 py-2 h-10 rounded-md flex items-center justify-center'
+                )}>
+                  {tab.title}
+                  {typeof tab.count !== undefined && tab.count !== null ? (
                   <span
                     className={classNames(
-                      tab.name === activeTab ? 'bg-main/20 text-main-secondary' : 'bg-gray-100 text-gray-900',
-                      'hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block'
+                      tab.name === activeTab ? 'bg-white/30 text-white' : 'bg-gray-100 text-gray-900',
+                      'hidden ml-2 h-6 w-6 rounded-full text-xs font-medium md:flex items-center justify-center'
                     )}
                   >
                     {tab.count}
                   </span>
                 ) : null}
+                </span>
               </a>
             ))}
           </nav>
