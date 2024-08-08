c3bbc7efcca462a50889debb2e3d7fc9ac8d8e35
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-08-08 02:15:29 +0100

    Remove underline from `Tabs` component

--- components/common/containers/Tabs.tsx
+++ components/common/containers/Tabs.tsx
@@ -22,41 +22,39 @@ const Tabs = ({tabs, className, activeTab, setActiveTab}) => {
         </select>
       </div>
       <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
-          <nav className="-mb-px flex space-x-4" aria-label="Tabs">
-            {tabs.map((tab, index) => (
-              <a
-                key={tab.name}
-                // href='#'
-                onClick={() => setActiveTab(tab.name)}
-                className={classNames(
-                  tab.name === activeTab
-                    ? 'border-main text-main-secondary'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200',
-                  'whitespace-nowrap flex py-2 font-medium text-sm cursor-pointer items-center'
-                )}
-                aria-current={tab.name === activeTab ? 'page' : undefined}
-              >
-                <span className={classNames(
-                  tab.name === activeTab ? 'bg-main-lightness-65 text-white' : 'bg-main-lightness-95 border-transparent text-gray-500',
-                  'px-3 py-2 h-10 rounded-md flex items-center justify-center'
-                )}>
-                  {tab.title}
-                  {typeof tab.count !== 'undefined' && tab.count !== null ? (
-                  <span
-                    className={classNames(
-                      tab.name === activeTab ? 'bg-white/30 text-white' : 'bg-gray-100 text-gray-900',
-                      'hidden ml-2 h-6 w-6 rounded-full text-xs font-medium md:flex items-center justify-center'
-                    )}
-                  >
-                    {tab.count}
-                  </span>
-                ) : null}
+        <nav className="-mb-px flex space-x-4" aria-label="Tabs">
+          {tabs.map((tab, index) => (
+            <a
+              key={tab.name}
+              // href='#'
+              onClick={() => setActiveTab(tab.name)}
+              className={classNames(
+                tab.name === activeTab
+                  ? 'border-main text-main-secondary'
+                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200',
+                'whitespace-nowrap flex py-2 font-medium text-sm cursor-pointer items-center'
+              )}
+              aria-current={tab.name === activeTab ? 'page' : undefined}
+            >
+              <span className={classNames(
+                tab.name === activeTab ? 'bg-main-lightness-65 text-white' : 'bg-main-lightness-95 border-transparent text-gray-500',
+                'px-3 py-2 h-10 rounded-md flex items-center justify-center'
+              )}>
+                {tab.title}
+                {typeof tab.count !== 'undefined' && tab.count !== null ? (
+                <span
+                  className={classNames(
+                    tab.name === activeTab ? 'bg-white/30 text-white' : 'bg-gray-100 text-gray-900',
+                    'hidden ml-2 h-6 w-6 rounded-full text-xs font-medium md:flex items-center justify-center'
+                  )}
+                >
+                  {tab.count}
                 </span>
-              </a>
-            ))}
-          </nav>
-        </div>
+              ) : null}
+              </span>
+            </a>
+          ))}
+        </nav>
       </div>
     </div>
   )
