cc2029f4ac85c719d2b4d1a8f6e13bf5526c8088
Mark Oxley <mark@idigitalmarketing.co.uk>
2024-08-09 14:31:03 +0100

    Don't show 'Loading' in `TableActions` when no rows available - base it upon `isLoading` state instead

--- components/common/tables/TableActions.tsx
+++ components/common/tables/TableActions.tsx
@@ -28,6 +28,7 @@ const TableActions = ({ table }: { table: Table<any> }) => {
   const types = useTableContext(s => s.typeOptions)
   const typeName = useTableContext(s => s.typeName)
   const dontShowTypes = useTableContext(s => s.dontShowTypes)
+  const isLoading = useTableContext(s => s.isLoading)
   
   const { tenantFeaturesEnabled } = useTenantFeaturesEnabled()
   const { userHasCapability } = useUserHasCapability()
@@ -157,7 +158,7 @@ const TableActions = ({ table }: { table: Table<any> }) => {
           <span className={`text-main-secondary whitespace-nowrap hover:text-main p-1 px-3 cursor-pointer`} onClick={clearFilters}>clear filters</span>
         )}
       </div>
-      <p className='whitespace-nowrap pl-3'>{!!tableData.length ? itemCountString : <>Loading<BlinkingEllipsis /></>}</p>
+      <p className='whitespace-nowrap pl-3'>{isLoading ? <>Loading<BlinkingEllipsis /></> : itemCountString}</p>
     </div>
   )
 }
