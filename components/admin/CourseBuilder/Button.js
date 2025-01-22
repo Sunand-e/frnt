61ea7a6fed35d741731473ce6f70071c26a6088e
Mark Oxley <mark@idigitalmarketing.co.uk>
2025-01-22 11:59:57 +0000

    refactor: Remove unused CourseBuilder components and misc. redundant files for cleaner codebase

--- components/admin/CourseBuilder/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({onClick, className, children}) => (
-  <button 
-    className={`${className} border-2 border-main text-main-secondary uppercase p-2 font-semibold`}
-    onClick={onClick}
-  >
-    {children}
-  </button>
-)
-export default Button
--- components/admin/CourseBuilder/Sidebar.js
+++
@@ -1,9 +0,0 @@
-export default function Sidebar({children}) {
-
-  return (
-    <div className="flex-none flex-col w-3/12 ml-8 align-stretch">
-      {children}
-    </div>
-  )
-
-}
