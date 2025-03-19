fa24097ffa20e16fd470e8b0a55d6d83ba6b35fb
Sunand-e <sunandkumar@digiryte.com>
2025-03-19 18:01:51 +0530

    #229 Hide Assigned Content tab for organization

--- components/groups/GroupForm.tsx
+++ components/groups/GroupForm.tsx
@@ -3,12 +3,9 @@ import { useForm } from 'react-hook-form';
 import { useDebouncedCallback } from 'use-debounce';
 import useGetGroup from '../../hooks/groups/useGetGroup';
 import useUpdateGroup from '../../hooks/groups/useUpdateGroup';
-import { handleModal } from '../../stores/modalStore';
 import { disableSubmitOnEnterKey } from '../../utils/forms';
 import { useRouter } from '../../utils/router';
-import Button from '../common/Button';
 import Tabs from '../common/containers/Tabs';
-import NumberPropertyInput from '../common/inputs/NumberPropertyInput';
 import TextInput from '../common/inputs/TextInput';
 import GroupContent from './GroupContent';
 import GroupUsers from './GroupUsers';
@@ -17,57 +14,60 @@ import { groupTypes } from '../common/groupTypes';
 import GroupCredits from './inputs/GroupCredits';
 interface GroupFormValues {
   id?: string
-  name: string 
+  name: string
   email: string
   groupImage: string
   userRole: string
   isOrganisation: boolean
-  // creditsUsed: number
-  // creditTotal: number
 }
 
-const GroupForm = ({typeName='group'}) => {
+const GroupForm = ({ typeName = 'group' }) => {
 
   const type = groupTypes[typeName]
 
   const router = useRouter()
   const { id } = router.query
 
-  const { group, loading } = useGetGroup(id)
+  const { group } = useGetGroup(id)
   const { updateGroup } = useUpdateGroup(id)
-  
+
   const debouncedUpdate = useDebouncedCallback((values) => {
     updateGroup(values);
   }, 500);
-  
+
   const { register, setFocus } = useForm<GroupFormValues>(
     {
       defaultValues: {
         ...group,
         name: (group.name === `Untitled ${type.name}`) ? '' : group.name,
-        // creditsUsed: group?.creditsUsed || 0,
-        // creditTotal: group?.creditTotal || 0,
       }
     }
   );
-  
+
   const [activeAssociatedContentTab, setActiveAssociatedContentTab] = useState('assigned')
   const [activeGroupUsersTab, setActiveGroupUsersTab] = useState('members')
 
-  const associatedContentTabs = [
-    {name: 'assigned', title: 'Assigned Content'},
-    {name: 'provided', title: 'Provided Content'}
-  ]
+  const [associatedContentTabs, setAssociatedContentTabs] = useState([
+    { name: 'assigned', title: 'Assigned Content' },
+    { name: 'provided', title: 'Provided Content' }
+  ])
   const groupUsersTabs = [
-    {name: 'members', title: 'Members'},
-    {name: 'leaders', title: 'Leaders'}
+    { name: 'members', title: 'Members' },
+    { name: 'leaders', title: 'Leaders' }
   ]
 
 
   useEffect(() => {
     setFocus('name')
-  },[])
-   
+    if (typeName == 'organisation') {
+      setActiveAssociatedContentTab('provided')
+
+      setAssociatedContentTabs([
+        { name: 'provided', title: 'Provided Content' }
+      ])
+    }
+  }, [])
+
   return (
     <form
       className='h-full w-full max-w-3xl flex flex-col space-y-4'
@@ -78,15 +78,15 @@ const GroupForm = ({typeName='group'}) => {
         placeholder={`${type.label} name`}
         inputAttrs={{
           ...register("name", { maxLength: 100 }),
-          onChange: (e => debouncedUpdate({ name: e.target.value }))
+          onChange: ((e: any) => debouncedUpdate({ name: e.target.value }))
         }}
       />
-      
+
       {typeName === 'organisation' && <GroupCredits group={group} />}
 
       <div>
         <Tabs activeTab={activeGroupUsersTab} setActiveTab={setActiveGroupUsersTab} tabs={groupUsersTabs} />
-        {activeGroupUsersTab === 'members' && <GroupUsers typeName={typeName} showRoles={['Member']} />}
+        {activeGroupUsersTab === 'members' && <GroupUsers showRoles={['Member']} />}
         {activeGroupUsersTab === 'leaders' && (
           <GroupUsers
             title={`${type.label} Leaders`}
@@ -104,16 +104,16 @@ const GroupForm = ({typeName='group'}) => {
           setActiveTab={setActiveAssociatedContentTab}
           tabs={associatedContentTabs}
         />
-        { activeAssociatedContentTab === 'assigned' && (
+        {activeAssociatedContentTab === 'assigned' && (
           <>
             <span className='flex items-start text-sm mb-2'>
-            <InformationCircle className='w-6 h-6 -mt-0.5 mr-2 text-main-lightness-70 shrink-0' />
+              <InformationCircle className='w-6 h-6 -mt-0.5 mr-2 text-main-lightness-70 shrink-0' />
               <p className='text-sm mb-2'>When content is 'assigned' to {type.withIndefiniteArticle}, all {type.name} members will gain access to that content.</p>
             </span>
             <GroupContent typeName='content' associationTypeName='assigned' />
           </>
         )}
-        { activeAssociatedContentTab === 'provided' && (
+        {activeAssociatedContentTab === 'provided' && (
           <>
             <span className='flex items-start text-sm mb-2'>
               <InformationCircle className='w-6 h-6 -mt-0.5 mr-2 text-main-lightness-70 shrink-0' />
