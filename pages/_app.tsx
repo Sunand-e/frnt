19b436d0ebeb847562b8d5833e95183a07b7497c
Sunand-e <sunandkumar@digiryte.com>
2025-03-27 14:50:52 +0530

    #183 Add versioning feature with automatic update checks and version.json generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,7 @@ import useBeforeUnload from '../hooks/useBeforeUnload';
 import CapabilityCheckWrapper from '../components/app/CapabilityCheckWrapper'
 import Modal from '../components/common/Modal'
 import { useViewStore } from '../hooks/useViewStore'
+import useVersionChecker from '../hooks/useVersionChecker'
 addIconsToLibrary()
 
 interface PagePropertiesType {
@@ -50,6 +51,8 @@ interface PagePropertiesType {
 type AppPropsExtended = AppProps & PagePropertiesType 
 
 const App = ({ Component: PageComponent, pageProps }: AppPropsExtended) => {
+
+  useVersionChecker();
 
   const router = useRouter()
   
